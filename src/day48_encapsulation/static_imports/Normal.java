bc1ee357c09eaf38eefcd25113b4336072f95d1c
alidokumac1 <alidokumaci1993@gmail.com>
2022-05-03 19:43:48 -0400

    day_48

--- src/day48_encapsulation/static_imports/Normal.java
+++ src/day48_encapsulation/static_imports/Normal.java
@@ -8,7 +8,7 @@ public class Normal {
 
     public static void main(String[] args) {
 
-        int[] a= {4,12,5,25};
+        int[] a = {4, 12, 5, 25};
         Arrays.sort(a);
 
         System.out.println(Arrays.toString(a));
